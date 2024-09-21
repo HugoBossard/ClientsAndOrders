dff164484160a33ab1e1f2372eb5673ff6d65407
Hugo Bossard <hbossard01@gmail.com>
2024-09-21 14:46:47 +0200

    Modify the name of app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'ClientsAndOrders'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('ClientsAndOrders');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ClientsAndOrders');
   });
 });
